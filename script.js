cd3f35fa1579001354548f7eabce94972e9bb438
AF1QUE <afique.dev@gmail.com>
2022-01-09 16:38:36 +0600

    Added smooth scroll feature to sections

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Slick slider feature
+
 $(".testimonials").slick({
   dots: true,
   infinite: true,
@@ -34,8 +36,33 @@ $(".testimonials").slick({
   ],
 });
 
+// Sticky header feature
+
 window.addEventListener("scroll", function () {
   var header = document.querySelector(".header");
   header.classList.toggle("sticky", window.scrollY > 0);
   console.log("fired!!!");
+});
+
+// Smooth scrolling feature
+
+// handle links with @href started with '#' only
+$(document).on("click", 'a[href^="#"]', function (e) {
+  // target element id
+  var id = $(this).attr("href");
+
+  // target element
+  var $id = $(id);
+  if ($id.length === 0) {
+    return;
+  }
+
+  // prevent standard hash navigation (avoid blinking in IE)
+  e.preventDefault();
+
+  // top position relative to the document
+  var pos = $id.offset().top;
+
+  // animated top scrolling
+  $("body, html").animate({ scrollTop: pos });
 });
